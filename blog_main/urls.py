5f0b2a68c10bdefd5cb4153818f9b664a76bd7e6
ankitpatelog <ankitpatel22092004@gmail.com>
2026-02-19 00:16:32 +0530

    added admin panel handling files and blog

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -8,5 +8,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',view.home,name='home')
+    path('',view.home,name='home'),
 ]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
