f0213c328c8468ffe8bb69ce95f2a79ff582a969
ankitpatelog <ankitpatel22092004@gmail.com>
2026-02-24 23:24:32 +0530

    added auth system

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -7,13 +7,14 @@ from django.conf import settings
 from django.conf.urls.static import static 
 from blogs import views as BlogViews
 
-
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',view.home,name='home'),
     path('category/',include('blogs.url')),
     # this one to show the blog per slug
     path('blog/',include('blogs.url')),
-    path('blog/<slug:slug>/', BlogViews.blog ),
+    path('blog/<slug:slug>/',include(BlogViews.blog)),
+    path('register/',view.register,name='register'),
+    path('login/',view.login,name='login'),
+    path('logout/',view.logout,name='logout'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- blogs/views.py
+++ blogs/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from . models import Blog,Category,Comment
+from .form import registrationform
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import auth
+
+
 # Create your views here.
 
 def posts_by_category(request,category_id):
@@ -43,3 +48,52 @@ def blog(request,slug):
     }
     
     return render(request,'blog.html',context)
+
+
+def register(request):
+    if request.method == 'POST':
+        # pass the form data stored into the request.post , then check for is_valid 
+        # if valid then save into the data base and then redirect
+        form = registrationform(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('register')
+        else:
+            print(form.errors)
+    else:
+        form = registrationform()
+        
+    context = {
+        'form':form,
+    }
+    
+    return render(request,'register.html',context)
+
+def login(request):
+    if request.method == 'POST':
+        # we will use defalut authentication form by django
+        form = AuthenticationForm(request,request.POST)
+        if form.is_valid():
+            # if form is valid take the user enterd fields
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            
+            # now take the auth from djanog
+            user = auth.authenticate(username=username,password=password)
+            
+            if user is not None:
+                auth.login(request,user)
+            return redirect('home')
+    
+    
+    # if it was not post request just view page then show the form 
+    form = AuthenticationForm()
+    context = {
+        'form' : form
+    }
+    return render(request, 'login.html',context) 
+
+
+def logout(request):
+    auth.logout()
+    return redirect('home')
